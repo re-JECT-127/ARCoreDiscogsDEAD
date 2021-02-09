b449520eeff77d3191a4cffa36a13917037bdd27
BuildTools <unconfigured@null.spigotmc.org>
2021-02-09 10:27:38 +0200

    More Green

--- app/src/main/java/com/example/arcorediscogs/TrackingFragment.kt
+++ app/src/main/java/com/example/arcorediscogs/TrackingFragment.kt
@@ -1,4 +1,52 @@
 package com.example.arcorediscogs
 
-class TrackingFragment {
+import android.graphics.BitmapFactory
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.google.ar.core.AugmentedImageDatabase
+import com.google.ar.core.Config
+import com.google.ar.core.Session
+import com.google.ar.sceneform.ux.ArFragment
+
+class TrackingFragment : ArFragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater, container:
+        ViewGroup?, savedInstanceState: Bundle?
+    ): View? {
+        val view = super.onCreateView(
+            inflater, container,
+            savedInstanceState
+        )
+        // Disable plane renderer and turn off planeDiscoveryController
+        planeDiscoveryController.hide()
+        planeDiscoveryController.setInstructionView(null)
+        arSceneView.planeRenderer.isEnabled = false
+        return view
+    }
+
+    override fun getSessionConfiguration(session: Session?): Config {
+        val config = super.getSessionConfiguration(session)
+        // Create image database and set it as a part of session configuration
+        setupAugmentedImageDatabase(config, session)
+        config.focusMode = Config.FocusMode.AUTO
+        return config
+    }
+
+    private fun setupAugmentedImageDatabase(
+        config: Config, session:
+        Session?
+    ) {
+        val augmentedImageDb = AugmentedImageDatabase(session)
+        val assetManager = context!!.assets
+        listOf("ree", "thetimlbr", "lagiacrus").forEach {
+            val inputStream = assetManager.open("$it.jpg")
+            val augmentedImageBitmap =
+                BitmapFactory.decodeStream(inputStream)
+            augmentedImageDb.addImage(it, augmentedImageBitmap)
+        }
+        config.augmentedImageDatabase = augmentedImageDb
+    }
+
 }
